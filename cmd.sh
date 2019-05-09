6a2933840ab982c6beeb2e10d5069fea83a15b94
AlekseyMartynov <AlekseyMartynov@users.noreply.github.com>
2019-05-09 18:41:56 +0300

    Skip short videos

--- cmd.sh
+++ cmd.sh
@@ -8,7 +8,7 @@ while true; do
             --audio-format mp3 \
             --audio-quality 5 \
             --playlist-items 1-3 \
-            --match-filter '!is_live' \
+            --match-filter '!is_live & duration > 299' \
             --download-archive '/ytpod/archive.txt' \
             --write-info-json \
             -o '/ytpod/public/%(upload_date)s-%(id)s.%(ext)s' \
